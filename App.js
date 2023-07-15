1ea215345cac88156f8172549b03d5c5ce6c98bd
msrinath <ashwinisrinath2580@gmail.com>
2023-07-15 19:59:38 +0530

    srinath

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my FIRST Commit</Text>
+      <Text>This is my FIRST Commit ok ok </Text>
       <StatusBar style="auto" />
     </View>
   );
