fc4ae508dc880a10981dcb4105c28dd09210a1c9
msrinath <ashwinisrinath2580@gmail.com>
2023-07-15 19:56:53 +0530

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my first Commit</Text>
+      <Text>This is my FIRST Commit</Text>
       <StatusBar style="auto" />
     </View>
   );
